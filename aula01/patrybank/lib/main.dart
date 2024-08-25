96d6a16c85f3a22cba970c5c77bebf3e370f11a9
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-08-25 19:49:41 -0300

    patrybank: tela de transferência

--- aula01/patrybank/lib/main.dart
+++ aula01/patrybank/lib/main.dart
@@ -1,36 +1,93 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      // Scaffold é o esqueleto
+  runApp(const PatryBankApp());
+}
+
+class PatryBankApp extends StatelessWidget {
+  const PatryBankApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       home: Scaffold(
+        body: FormularioTransferencia(),
+      ),
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  const FormularioTransferencia({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
+          backgroundColor: const Color(0xFF185A2D),
           title: const Text(
-            'Transferências',
+            'Criando Transferência',
             style: TextStyle(color: Colors.white),
           ),
-          backgroundColor: Colors.blue,
-        ), // AppBar é a parte de cima
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
         ),
-        body: ListaTransferencias(),
-      ),
-    ),
-  );
+        body: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(fontSize: 24.0),
+                decoration: InputDecoration(
+                    labelText: 'Número da conta',
+                    hintText: '0000' // placeholder
+                    ),
+                keyboardType:
+                    TextInputType.number, // transforma em teclado numérico
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+              child: TextField(
+                style: TextStyle(fontSize: 24.0),
+                decoration: InputDecoration(
+                    icon: Icon(Icons.monetization_on),
+                    labelText: 'Valor',
+                    hintText: '0.00' // placeholder
+                    ),
+                keyboardType:
+                    TextInputType.number, // transforma em teclado numérico
+              ),
+            ),
+            ElevatedButton(
+              child: const Text('Confirmar'),
+              onPressed: () {},
+            )
+          ],
+        ));
+  }
 }
 
 class ListaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ItemTransferencia(Transferencia(100, 12345)),
-        ItemTransferencia(Transferencia(200, 85554)),
-        ItemTransferencia(Transferencia(300, 9815)),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title:
+            const Text('Transferências', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          ItemTransferencia(Transferencia(100, 12345)),
+          ItemTransferencia(Transferencia(200, 85554)),
+          ItemTransferencia(Transferencia(300, 9815)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
